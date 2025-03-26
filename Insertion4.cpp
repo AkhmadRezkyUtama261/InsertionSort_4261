ed2373f05a802c745367203946d72caf4d04b396
AkhmadRezkyUtama261 <ahkmadrezkyutama369@gmail.com>
2025-03-26 13:03:59 +0700

    Menambahkan komentar pada fungsi display

--- Insertion4.cpp
+++ Insertion4.cpp
@@ -57,18 +57,18 @@ arr [j + 1] = temp; //step 5
 
 void display ()
 {
-cout << endl;
+cout << endl; //output baris kosong
 cout << "=================================" << endl;
-cout << "Total Passs = " << n - 1 << endl;
-cout << "=================================" << endl;
-cout << "Element Array yang telah tersusun" << endl;
-cout << "=================================" << endl;
+cout << "Total Passs = " << n - 1 << endl; //count element movement
+cout << "=================================" << endl; //output ke layar
+cout << "Element Array yang telah tersusun" << endl; //output ke layar
+cout << "=================================" << endl; //output ke layar
 
 for (int j = 0; j < n; j++)
-{
-cout << arr [j] << endl; 
+{ // Looping dengan j dimulai dari 0 hingga n-1
+cout << arr [j] << endl; //output ke layar
 }
-cout << endl;
+cout << endl; //output baris kosong
 }
 
  int main ()
