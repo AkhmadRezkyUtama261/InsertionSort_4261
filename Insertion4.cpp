247b49c097fafcaa75fb0fff2d36bfb43eaaf5eb
AkhmadRezkyUtama261 <ahkmadrezkyutama369@gmail.com>
2025-03-26 12:38:41 +0700

    Penambahan fungsi input

--- Insertion4.cpp
+++ Insertion4.cpp
@@ -3,3 +3,31 @@ using namespace std;
 
 int arr[20];
 int n;
+
+void input ()
+{
+while (true)
+{
+cout << "Masukkan Jumlah Data pada Array : ";
+cin >> n;
+
+if (n <=20)
+{
+    break;
+}
+else
+{
+    cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n";
+}
+}
+cout << endl;
+cout << "======================" << endl;
+cout << "Masukkan Element Array" << endl;
+cout << "======================" << endl;
+
+for (int i = 0; i < n; i++)
+{
+cout << "Data ke-" << (i + 1) << ": ";
+cin >> arr[i];
+}
+}
